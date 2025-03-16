a3e41b48e223e1b4599bc297d9fa80afd497fdd5
yana.radeva7 <yana.radeva7@gmail.com>
2025-03-16 18:55:15 +0200

    add CORS support in the API

--- API/Program.cs
+++ API/Program.cs
@@ -6,14 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
+builder.Services.AddDbContext<DataContext>(options =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(options =>
+{
+    options.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
 app.MapControllers();
 
 app.Run();
