e29fb414336b28251a04fe222978e3072ade8f56
yana.radeva7 <yana.radeva7@gmail.com>
2025-03-17 11:34:07 +0200

    add account controller with a register endpoint

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken!");  
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        } 
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(user => user.UserName.ToLower() == username.ToLower());
+        }
+    }
+}
