49b77cb8375badfecf622a3f4225994c11caa6c7
yana.radeva7 <yana.radeva7@gmail.com>
2025-03-23 18:02:36 +0200

    add custom exception middleware and a dummy controller to test diverse http error responses

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,42 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController(DataContext context) : BaseApiController
+    {
+
+        [HttpGet("bad-request")]
+        public IActionResult Get400Error() 
+        {
+            return BadRequest("Something bad has happened!");
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetAuth()
+        {
+            return "secret";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> Get404Error()
+        {
+            var user = context.Users.Find(-1);
+
+            if (user == null) return NotFound("Couldn't find what you are looking for!");
+            
+            return user;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var user = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened!");
+
+            return user;
+        }
+    }
+}
