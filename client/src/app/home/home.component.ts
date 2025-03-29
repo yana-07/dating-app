899ee393a008ceaa6ef9db8bef03f06ba566be14
yana.radeva7 <yana.radeva7@gmail.com>
2025-03-29 20:59:31 +0200

    add member-card component and member service; extend member-list component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
 
 import { RegisterComponent } from "../register/register.component";
 
@@ -10,28 +9,14 @@ import { RegisterComponent } from "../register/register.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   registerMode = false;
-  users: any;
-  private http = inject(HttpClient);
 
-  ngOnInit() {
-    this.getUsers();
-  }
-  
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed.')
-    });
   }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+
+import { MemberService } from '../../_services/member.service';
+import { Member } from '../../_models/member';
+import { MemberCardComponent } from '../member-card/member-card.component';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit{
+  members = signal<Member[]>([]);
+  private memberService = inject(MemberService);
 
+  ngOnInit() {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: (members) => this.members.set(members)
+    });
+  }
 }
