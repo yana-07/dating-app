a694f3e5e9704b95909b028e7b9831112f9f85cf
yana.radeva7 <yana.radeva7@gmail.com>
2025-03-29 21:30:30 +0200

    add interceptor to set up authorization bearer token header

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -1,9 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
-import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,27 +10,12 @@ import { AccountService } from './account.service';
 export class MemberService {
   baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
 
   getMembers() {
-    return this.http.get<Member[]>(
-      this.baseUrl + 'users',
-      this.getHttpOptions()
-    );
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(
-      this.baseUrl + 'users/' + username,
-      this.getHttpOptions()
-    );
-  }
-
-  getHttpOptions() {
-    return {
-      headers: new HttpHeaders({
-        Authorization: `Bearer ${this.accountService.currentUser()?.token}`,
-      }),
-    };
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
