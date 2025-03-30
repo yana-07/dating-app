bc089c5b68f96040a6e59280ca169d3726d335af
yana.radeva7 <yana.radeva7@gmail.com>
2025-03-30 16:41:45 +0300

    use and style ngx-bootstrap's tabset and tab components; use ng-gallery's gallery component; serve static files and update favicon

--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 import { MemberService } from '../../_services/member.service';
 import { Member } from '../../_models/member';
@@ -7,12 +9,13 @@ import { Member } from '../../_models/member';
 @Component({
   selector: 'app-member-details',
   standalone: true,
-  imports: [],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-details.component.html',
   styleUrl: './member-details.component.css',
 })
 export class MemberDetailsComponent implements OnInit {
   member?: Member;
+  images: GalleryItem[] = [];
   private route = inject(ActivatedRoute);
   private memberService = inject(MemberService);
 
@@ -25,7 +28,12 @@ export class MemberDetailsComponent implements OnInit {
     if (!username) return;
 
     this.memberService.getMember(username).subscribe({
-      next: (member) => (this.member = member),
+      next: (member) => {
+        this.member = member;
+        member.photos.forEach(p => {
+          this.images.push(new ImageItem({src: p.url, thumb: p.url}));
+        });
+      }
     });
   }
 }
