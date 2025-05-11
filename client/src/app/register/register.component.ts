1edddc61d829acb4187e368ac8c1811965a23bdd
yana.radeva7 <yana.radeva7@gmail.com>
2025-05-11 22:14:13 +0300

    switch to reactive forms in the register component; create custom validator; add validation feedback text

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,38 +1,71 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
 
-import { User } from '../_models/user.model';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   model: any = {};
-  //@Input({required: true}) users!: User[];
-  //@Output() cancelReg = new EventEmitter<boolean>(); 
-  //users = input.required<any>();
   cancelReg = output<boolean>();
+  registerForm: FormGroup = new FormGroup({});
   private accountService = inject(AccountService);
   private toastr = inject(ToastrService);
 
-  register () {
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password')
+      ]),
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return (control.value === control.parent?.get(matchTo)?.value) 
+        ? null
+        : { isNotMatching: true };
+    }
+  }
+
+  register() {
     this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
+      next: () => this.cancel(),
+      error: (error) => this.toastr.error(error.error),
     });
   }
 
   cancel() {
-    console.log('cancelled');
     this.cancelReg.emit(false);
   }
 }
